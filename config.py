a878bbd51ce8d270877cb7ffa665daf5a731c4cf
Artoym1 <moisar@yandex.ru>
2023-05-18 21:14:32 +0300

    Переработана логика диалога с пользователем

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-comunity_token = 'vk1.a.cJ7vjhYOsOYA53yf_NREO7gJldLtXZ-vdylxmtstwRN9qEXGQe4s7Xs9VEDgAfdG7KIxfrIBE6o6eAH7ee4gY_40dGZuuWBvX3RbjsBlzNHERlw-4Jdd-3U2TRvepcU-1a2bRb-mZw8RP7mvmTlUe3jHbOBTvOVqXM9gciURsnFsLrX9-MZgei6S2uhyLhtlJ5VJBfmIWmjadO29V8anrA'
-access_token = 'vk1.a.mvTgH92HHzsk2pjca_3UYSn8eoe3SJiemOUd0wuLuMTlCDIxf7shsU0Ia0EMXY_WS7Q5vvZQsIvA5Nzsg_dOFb-TJANm3RgJA359UUs7l8AV_JmYzHnC1g1z9E-K-zVz5kR8kHwlMsrCexJ2uZrqOxWI_wJJdl-lHHrlweIhlYE4o3mm5kO6kfgg1r_Jyp4DAQN1o-MF-ZnP9xItF7uJNg'
+comunity_token = ''
+access_token = ''
 
-db_url_object = "postgresql+psycopg2://postgres:123@localhost:5432/Viewed"
+db_url_object = "postgresql+psycopg2://postgres:___@localhost:5432/___"
